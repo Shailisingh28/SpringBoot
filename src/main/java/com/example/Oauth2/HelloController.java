3f35a58e1ad0fb3bbac3c0b46303f43ec07d95f5
Shailisingh28 <shaili2809@gmail.com>
2026-03-29 18:20:54 +0530

    Oauth2

---
+++ src/main/java/com/example/Oauth2/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.Oauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello Oauth2";
+    }
+}
